f65fed644b403d03919e81c585c9d2d5853c3948
fechilli <luizfdas@outlook.com>
2022-06-29 23:30:45 -0300

    comecando bd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('../mysql').pool;
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
   res.render('/login.html');
+});
+//insere um usuario
+router.get('/', (req, res, next) => {
+
+
+    mysql.getConnection((error, conn) => {
+        conn.query(
+          'INSERT INTO user (nome,senha) VALUES(?,?)',
+          [req.body.nome, req.body.senha],
+          (error, resultado, field) =>{
+            conn.release();
+            if (error){
+              res.status(500).send({ 
+                error: error,
+                response: null
+              })
+            }
+          }
+      )
+    },
+
+  res.status(201).send({
+    mensagem: "Insere um usuario",
+    usuarioCriado: user
+  })
 });
 
 module.exports = router;
